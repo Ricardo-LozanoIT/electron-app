a315076a35c1121e49f00974fd620de5f0aeda4c
magualimpia <melanie@ayte.co>
2024-06-06 17:00:47 -0500

    [✨Feature]: modification to make the dynamo work

--- AYTE/demo-abbott/infrastructure/src/demoAbbottFunction/index.js
+++ AYTE/demo-abbott/infrastructure/src/demoAbbottFunction/index.js
@@ -1,8 +1,14 @@
 import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
+import {
+  PutCommand,
+  DynamoDBDocumentClient,
+  GetCommand,
+} from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({});
 
 export const getItem = async (id) => {
+  console.log("before")
   const command = new GetItemCommand({
     "TableName": "demo-abbott-demoAbbott-1TMM2C43BCIKL",
     "Key": {
@@ -15,11 +21,16 @@ export const getItem = async (id) => {
   return response;
 };
 
-export const handler = async (event) => {
+export const handler = async (event, context, callback) => {
   // Create a put event to send data to dynamoDB
   const id = "123456789";
   await getItem(id);
   console.log("event", event);
+  if(event.httpMethod == 'GET'){
+    console.log("esto es un GET")
+  } else if(event.httpMethod == 'PUT'){
+    console.log("esto es un PUT")
+  }
 
   let res = {
     statusCode: 200,
