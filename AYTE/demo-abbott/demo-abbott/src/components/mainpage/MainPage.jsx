aeebdd64b7ef58f1d07917c759653deb658b6cc7
Ricardo Lozano <ricardo@ayte.co>
2024-06-11 08:45:39 -0500

    🦖[UPDATE] design better and fix margins

--- AYTE/demo-abbott/demo-abbott/src/components/mainpage/MainPage.jsx
+++ AYTE/demo-abbott/demo-abbott/src/components/mainpage/MainPage.jsx
@@ -6,6 +6,8 @@ import axios from "axios";
 export const MainPage = () => {
   const [code, setCode] = useState(false);
   const [id, setId] = useState("");
+  const [codeNumber, setCodeNumber] = useState("");
+  const [validateCode, setValidateCode] = useState(false);
 
   const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
@@ -14,8 +16,24 @@ export const MainPage = () => {
   const getAutentication = async (id) => {
     const resp = await axios.get(
       "https://93buay9mm1.execute-api.us-east-1.amazonaws.com/Prod",
-      { params: { id }}
+      { params: { id } }
     );
+    console.log(resp?.data?.allow);
+    setCode(resp?.data?.allow);
+  };
+
+  const getValidateCode = async (id, code) => {
+    const resp = await axios.get(
+      "https://93buay9mm1.execute-api.us-east-1.amazonaws.com/Prod/code",
+      { params: { id, code } }
+    );
+    console.log(resp?.data?.allow);
+    setValidateCode(resp?.data?.allow);
+    if (resp?.data?.allow) {
+      alert("Bienvenido a Abbott");
+    } else {
+      alert("codigo ingresado incorrecto");
+    }
   };
 
   return (
@@ -24,7 +42,11 @@ export const MainPage = () => {
         <div className={styles.inputsForm}>
           <div className={styles.input}>
             <span>Numero de Identificacion</span>
-            <input type="text" maxLength={10} />
+            <input
+              type="text"
+              maxLength={10}
+              onChange={(value) => setId(value?.target?.value)}
+            />
           </div>
         </div>
         {code ? (
@@ -33,24 +55,32 @@ export const MainPage = () => {
               Ingrese el codigo que fue enviado a su celular o correo
             </span>
             <div className={styles.container_codeNumber}>
-              <input type="text" maxLength={1} />
-              <input type="text" maxLength={1} />
-              <input type="text" maxLength={1} />
-              <input type="text" maxLength={1} />
-              <input type="text" maxLength={1} />
-              <input type="text" maxLength={1} />
+              <input
+                type="text"
+                maxLength={6}
+                onChange={(value) => setCodeNumber(value?.target?.value)}
+              />
             </div>
           </div>
         ) : (
           <></>
         )}
         <div className={styles.container_button}>
-          <button
-            className={styles.button}
-            onClick={() => getAutentication(12345)}
-          >
-            Ingresar
-          </button>
+          {!code ? (
+            <button
+              className={styles.button}
+              onClick={() => getAutentication(id)}
+            >
+              Ingresar
+            </button>
+          ) : (
+            <button
+              className={styles.button}
+              onClick={() => getValidateCode(id, codeNumber)}
+            >
+              Validar
+            </button>
+          )}
         </div>
       </div>
     </>
